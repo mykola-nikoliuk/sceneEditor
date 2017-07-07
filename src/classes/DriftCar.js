7f190f92c52a7b7d6f61c798d580a4c046d62e6c
Nick Nikoliuk <mykola.nikoliuk@gmail.com>
2017-07-07 10:18:11 +0300

    car, cubemap added

---
+++ src/classes/DriftCar.js
@@ -0,0 +1,71 @@
+import THREE from "../lib/three";
+import Car from "../classes/car/Car";
+import each from "lodash/each";
+
+import bmwOBJ from "../resources/bmwm3/BMW_M3_GTR.obj";
+import bmwMTL from "../resources/bmwm3/BMW_M3_GTR.mtl";
+import wheelOBJ from "../resources/wheel/disk_g.obj";
+import wheelMTL from "../resources/wheel/disk_g.mtl";
+
+const {PI} = Math;
+
+export default new Promise(response => {
+
+    THREE.OBJMTLLoader.load(bmwOBJ, bmwMTL, 'resources/bmwm3/').then(carMesh => {
+        const scale = 0.1;
+        carMesh.scale.set(scale, scale, scale);
+        let i = 0;
+        each(carMesh.children, mesh => {
+            mesh.receiveShadow = true;
+            mesh.castShadow = true;
+        });
+        // setInterval(() => {
+        //     bmw.children[i].visible = true;
+        //     if (i >= bmw.children.length - 1) {
+        //         index = -1;
+        //     }
+        //     bmw.children[++i].visible = false;
+        //     console.log(i);
+        // }, 500);
+        [30, 31, 36, 37, 42, 43, 48, 49].forEach(index => {
+            carMesh.children[index].visible = false;
+        });
+        carMesh.children[32].transparent = true;
+        // bmw.rotation.y = PI + PI / 2;
+
+        THREE.OBJMTLLoader.load(wheelOBJ, wheelMTL, 'resources/wheel/').then(wheelMesh => {
+            const scale = 2200;
+            wheelMesh.scale.set(scale, scale, scale);
+            response(createCar(carMesh, wheelMesh));
+        });
+    });
+});
+
+function createCar(carMesh, wheelMesh) {
+    // const wheelGroup = new THREE.Group();
+    // wheelGroup.add(wheelMesh);
+
+    const car = new Car(carMesh, {
+        axels: [{
+            mesh: wheelMesh,
+            width: 5000,
+            maxAngle: PI / 3,
+            camber: (-5).toRadians(),
+            convergence: (.5).toRadians(),
+            maxAngleCamber: 0,
+            rotationPointOffset: 30,
+            offset: new THREE.Vector2(5280, 1080)
+        }, {
+            mesh: wheelMesh,
+            width: 5000,
+            offset: new THREE.Vector2(-3700, 1100)
+        }]
+    });
+
+    car.mesh.castShadow = true;
+    car.mesh.receiveShadow = true;
+
+    car.mesh.rotation.y = PI / 2;
+
+    return car;
+}
--- src/index.js
+++ src/index.js
@@ -1,114 +1,71 @@
 import THREE from "./lib/three";
 import "./style/index.styl";
-import Car from "./classes/car/Car";
 import "./utils/utils"
 import keyboard from "./classes/Keyboard";
-
-import bmwOBJ from "./resources/bmwm3/BMW_M3_GTR.obj";
-import bmwMTL from "./resources/bmwm3/BMW_M3_GTR.mtl";
-import wheelOBJ from "./resources/wheel/disk_g.obj";
-import wheelMTL from "./resources/wheel/disk_g.mtl";
+import carPromise from "./classes/DriftCar";
 
 const {PI} = Math;
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true});
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000000);
 const scene = new THREE.Scene();
-const aLight = new THREE.AmbientLight(0xffffff, 0.5);
-const pLight = new THREE.DirectionalLight(0xffffff, 1);
+const aLight = new THREE.AmbientLight(0xffffff, 1);
+const pLight = new THREE.SpotLight(0xffffff, 1, 5000, 0.5);
 
 let previousTimestamp = 0;
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// const car = createCar();
 
-let bmw, car, wheel;
-THREE.OBJMTLLoader.load(bmwOBJ, bmwMTL, 'resources/bmwm3/').then(obj => {
-    scene.add(obj);
-    bmw = obj;
-    const scale = 0.1;
-    bmw.scale.set(scale, scale, scale);
-    let i = 0;
-    // setInterval(() => {
-    //     bmw.children[i].visible = true;
-    //     if (i >= bmw.children.length - 1) {
-    //         index = -1;
-    //     }
-    //     bmw.children[++i].visible = false;
-    //     console.log(i);
-    // }, 500);
-    [30, 31, 36, 37, 42, 43, 48, 49].forEach(index => {
-        bmw.children[index].visible = false;
-    });
-    bmw.children[32].transparent = true;
-    // bmw.rotation.y = PI + PI / 2;
-
-    THREE.OBJMTLLoader.load(wheelOBJ, wheelMTL, 'resources/wheel/').then(obj => {
-        const scale = 2200;
-        wheel = obj;
-        wheel.scale.set(scale, scale, scale);
-        car = createCar();
-        render(previousTimestamp);
-    });
-});
-
-
-scene.add(aLight);
+pLight.position.y = 1000;
+pLight.position.x = 2000;
+pLight.castShadow = true;
 scene.add(pLight);
+scene.add(aLight);
 
-camera.position.set(1300, 1300, 1200);
-camera.lookAt(new THREE.Vector3(0,0,0));
+const cubemapImages = [
+  'resources/cubemap/right.jpg',
+  'resources/cubemap/left.jpg',
+  'resources/cubemap/top.jpg',
+  'resources/cubemap/bottom.jpg',
+  'resources/cubemap/front.jpg',
+  'resources/cubemap/back.jpg'
+];
+scene.background = new THREE.CubeTextureLoader().load(cubemapImages);
 
 renderer.setClearColor(0x222222);
-renderer.shadowMapEnabled = true;
-renderer.shadowMapType = THREE.PCFSoftShadowMap;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-function createCar() {
-    const radius = 1200;
-    const wheelMaterial = new THREE.MeshStandardMaterial({color: 0xff0000, wireframe: !true});
+const asphaltTexture = new THREE.TextureLoader().load('resources/asphalt/asphalt.jpg');
+asphaltTexture.wrapT = THREE.RepeatWrapping;
+asphaltTexture.wrapS = THREE.RepeatWrapping;
+asphaltTexture.repeat.set(400, 400);
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(100000, 100000),
+    new THREE.MeshPhongMaterial({map: asphaltTexture})
+);
 
-    const wheelGeometry = new THREE.CylinderGeometry(radius, radius, 1000, 12, 1);
-    const wheelMesh = new THREE.Mesh(wheelGeometry, wheelMaterial);
-    wheelMesh.rotation.x = PI / 2;
-    const wheelGroup = new THREE.Group();
-    wheelGroup.add(wheel);
-
-    const car = new Car(bmw, {
-        axels: [{
-            mesh: wheelGroup,
-            width: 5000,
-            maxAngle: PI / 3,
-            camber: (-5).toRadians(),
-            convergence: (.5).toRadians(),
-            maxAngleCamber: 0,
-            rotationPointOffset: 30,
-            offset: new THREE.Vector2(5280, 1080)
-        }, {
-            mesh: wheelGroup,
-            width: 5000,
-            offset: new THREE.Vector2(-3700, 1100)
-        }]
-    });
-
-    car.mesh.castShadow = true;
-    car.mesh.receiveShadow = true;
+plane.receiveShadow = true;
+plane.rotation.x = -PI / 2;
+scene.add(plane);
 
 
-    car.mesh.rotation.y = PI / 2;
-    // car.mesh.position.z = 600;
-    scene.add(car.mesh);
 
-    return car;
-}
+let car;
+carPromise.then(driftCar => {
+    car = driftCar;
+    driftCar.mesh.position.y = -10;
+    scene.add(driftCar.mesh);
+    render(previousTimestamp);
+});
 
-const cubeG = new THREE.CubeGeometry(100, 100, 100, 1, 1, 1);
-const cubeM = new THREE.MeshStandardMaterial({color: 0xff0000});
-const cube = new THREE.Mesh(cubeG, cubeM);
-cube.castShadow = true;
-cube.position.y = 600;
-scene.add(cube);
+camera.lookAt(new THREE.Vector3());
+const cameraRotation = new THREE.Vector2();
+const R = 2000;
+
+
 
 
 function render(timestamp) {
@@ -125,8 +82,26 @@ function render(timestamp) {
     // if (S) camera.position.y -= cameraSpeed;
 
     window.requestAnimationFrame(render);
-    bmw.rotation.y += 0.005;
-    // car.position.z += 0.1;
+    // car.mesh.rotation.y += 0.005;
+    // car.mesh.position.z -= 2;
     car.update(delta);
+
+    // camera.rotation.copy(car.mesh.rotation);
+    // camera.position.set(
+    //     R * Math.cos(cameraRotation.x),
+    //     R * Math.sin(cameraRotation.x) * Math.sin(cameraRotation.y),
+    //     R * Math.sin(cameraRotation.x) * Math.cos(cameraRotation.y)
+    // ).add(car.mesh.position);
+    // cameraRotation.y = (cameraRotation.y + 0.001);
+    // cameraRotation.x = (cameraRotation.x + 0.01);
+    // cameraRotation.y = cameraRotation.y - 0.001  % Math.PI;
+    // camera.lookAt(car.mesh.position);
+    // cameraRotation.rotation.y += 0.01;
+
+    camera.position.x =
+    camera.position.y =
+    camera.position.z = 1500;
+    camera.lookAt(car.mesh.position);
+
     renderer.render(scene, camera);
 }
